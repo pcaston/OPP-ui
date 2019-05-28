f6cf4cef2f04e270950cfe52867b664da1bf30f7
Paul Caston <paul.caston@iag.com.au>
2019-05-28 17:26:38 +0800

    more ha

---
+++ src/layouts/open-peer-power.ts
@@ -0,0 +1,97 @@
+import "@polymer/app-route/app-location";
+import { html, LitElement, PropertyValues, css, property } from "lit-element";
+
+import "./open-peer-power-main";
+import "./op-init-page";
+import "../resources/op-style";
+import { registerServiceWorker } from "../util/register-service-worker";
+import { DEFAULT_PANEL } from "../common/const";
+
+import { Route, HomeAssistant } from "../types";
+import { navigate } from "../common/navigate";
+import { HassElement } from "../state/opp-element";
+
+(LitElement.prototype as any).html = html;
+(LitElement.prototype as any).css = css;
+
+export class HomeAssistantAppEl extends HassElement {
+  @property() private _route?: Route;
+  @property() private _error?: boolean;
+  @property() private _panelUrl?: string;
+
+  protected render() {
+    const opp = this.opp;
+
+    return html`
+      <app-location
+        @route-changed=${this._routeChanged}
+        ?use-hash-as-path=${__DEMO__}
+      ></app-location>
+      ${this._panelUrl === undefined || this._route === undefined
+        ? ""
+        : opp && opp.states && opp.config && opp.services
+        ? html`
+            <open-peer-power-main
+              .opp=${this.opp}
+              .route=${this._route}
+            ></open-peer-power-main>
+          `
+        : html`
+            <op-init-page .error=${this._error}></op-init-page>
+          `}
+    `;
+  }
+
+  protected firstUpdated(changedProps) {
+    super.firstUpdated(changedProps);
+    this._initialize();
+    setTimeout(registerServiceWorker, 1000);
+    /* polyfill for paper-dropdown */
+    import(/* webpackChunkName: "polyfill-web-animations-next" */ "web-animations-js/web-animations-next-lite.min");
+  }
+
+  protected updated(changedProps: PropertyValues): void {
+    if (changedProps.has("_panelUrl")) {
+      this.panelUrlChanged(this._panelUrl!);
+      this._updateHass({ panelUrl: this._panelUrl });
+    }
+    if (changedProps.has("opp")) {
+      this.hassChanged(this.opp!, changedProps.get("opp") as
+        | HomeAssistant
+        | undefined);
+    }
+  }
+
+  protected async _initialize() {
+    try {
+      const { auth, conn } = await window.hassConnection;
+      this.initializeHass(auth, conn);
+    } catch (err) {
+      this._error = true;
+      return;
+    }
+  }
+
+  private _routeChanged(ev) {
+    const route = ev.detail.value as Route;
+    // If it's the first route that we process,
+    // check if we should navigate away from /
+    if (
+      this._route === undefined &&
+      (route.path === "" || route.path === "/")
+    ) {
+      navigate(window, `/${localStorage.defaultPage || DEFAULT_PANEL}`, true);
+      return;
+    }
+
+    this._route = route;
+
+    const dividerPos = route.path.indexOf("/", 1);
+    this._panelUrl =
+      dividerPos === -1
+        ? route.path.substr(1)
+        : route.path.substr(1, dividerPos - 1);
+  }
+}
+
+customElements.define("open-peer-power", HomeAssistantAppEl);
---
+++ src/layouts/opp-loading-screen.ts
@@ -0,0 +1,62 @@
+import "@polymer/app-layout/app-toolbar/app-toolbar";
+import "@polymer/paper-spinner/paper-spinner-lite";
+import {
+  LitElement,
+  TemplateResult,
+  html,
+  CSSResultArray,
+  css,
+  customElement,
+  property,
+} from "lit-element";
+import "../components/op-menu-button";
+import "../components/op-paper-icon-button-arrow-prev";
+import { haStyle } from "../resources/styles";
+
+@customElement("opp-loading-screen")
+class HassLoadingScreen extends LitElement {
+  @property({ type: Boolean }) public rootnav? = false;
+
+  protected render(): TemplateResult | void {
+    return html`
+      <app-toolbar>
+        ${this.rootnav
+          ? html`
+              <op-menu-button></op-menu-button>
+            `
+          : html`
+              <op-paper-icon-button-arrow-prev
+                @click=${this._handleBack}
+              ></op-paper-icon-button-arrow-prev>
+            `}
+      </app-toolbar>
+      <div class="content">
+        <paper-spinner-lite active></paper-spinner-lite>
+      </div>
+    `;
+  }
+
+  private _handleBack() {
+    history.back();
+  }
+
+  static get styles(): CSSResultArray {
+    return [
+      haStyle,
+      css`
+        .content {
+          height: calc(100% - 64px);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `,
+    ];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "opp-loading-screen": HassLoadingScreen;
+  }
+}
