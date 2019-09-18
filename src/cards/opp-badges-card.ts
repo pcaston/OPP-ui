a827fc810c3bb57e3466aa0762d275f06321191f
Paul Caston <paul.caston@iag.com.au>
2019-09-18 16:59:07 +0800

    render template fixes

--- src/cards/opp-badges-card.ts
+++ src/cards/opp-badges-card.ts
@@ -2,15 +2,17 @@ import { html, property, customElement } from 'lit-element';
 
 import "../components/entity/opp-state-label-badge";
 import { OpenPeerPower } from '../types';
-import { OppEntities } from "../open-peer-power-js-websocket/lib";
+import { OppEntities, OppEntity} from "../open-peer-power-js-websocket/lib";
 import { PageViewElement } from '../components/page-view-element';
 
 @customElement("opp-badges-card")
 export class OppBadgesCard extends PageViewElement {
   @property({ type : Object }) opp!: OpenPeerPower;
   @property({ type : Array }) states?: OppEntities;
+
   protected render() {
     console.log("render opp-badges-card");
+
     return html`
       <style>
         opp-state-label-badge {
@@ -19,13 +21,11 @@ export class OppBadgesCard extends PageViewElement {
         }
       </style>
 
-      ${Object.keys(this.opp!.states!).map((key) => {
-        const item = this.opp!.states![key];
-        console.log("opp-badges render");
-        console.log(item);
+      ${this.states!.map((key) => {
+        const state = this.states![key];
         return html`
           <div>
-            <opp-state-label-badge opp="${JSON.stringify(this.opp)}" id="${item.entity_id}" state="${JSON.stringify(item)}"></opp-state-label-badge>
+            <opp-state-label-badge opp="${JSON.stringify(this.opp)}" id="${state.entity_id}" state="${JSON.stringify(state)}"></opp-state-label-badge>
           </div>
         `;
       })
