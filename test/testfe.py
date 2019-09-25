f0771622457f65a954e421890b652259f7a507ea
Paul Caston <paul.caston@iag.com.au>
2019-09-25 17:35:52 +0800

    new testfe

--- test/testfe.py
+++ test/testfe.py
@@ -31,21 +31,6 @@ async def notify_state():
 async def register(websocket):
     USERS.add(websocket)
 
-async def process(websocket, message):
-    msg = json.loads(message)
-    if msg['type'] == 'login' or msg['type'] == 'auth':
-        await websocket.send(json.dumps(
-            {'type': 'auth_ok',
-                'version': '0.1.0',
-                'access_token': ACCESS_TOKEN
-            }
-            ))
-    elif msg['type'] == 'get_states':
-        await notify_state()
-    else:
-        print("unsupported event: {}", msg)
-
-
 async def unregister(websocket):
     USERS.remove(websocket)
 
@@ -55,14 +40,24 @@ async def counter(websocket, path):
     await websocket.send(json.dumps(
         {'type': 'auth_required'}
     ))
-    try:
-        async for message in websocket:
-            await process(websocket, message)
-            
-    except websockets.ConnectionClosed:
-        print(f"Websocket Terminated")
-    finally:
-        await unregister(websocket)
+    while True:
+        try:
+            message = await websocket.recv()
+            msg = json.loads(message)
+            if msg['type'] == 'login' or msg['type'] == 'auth':
+                await websocket.send(json.dumps(
+                    {'type': 'auth_ok',
+                        'version': '0.1.0',
+                        'access_token': ACCESS_TOKEN
+                    }
+                ))
+            elif msg['type'] == 'get_states':
+                await notify_state()
+            else:
+                print("unsupported event: {}", msg)
+        except websockets.ConnectionClosed:
+            print(f"Websocket Terminated")
+            break
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(counter, 'localhost', 8123))
