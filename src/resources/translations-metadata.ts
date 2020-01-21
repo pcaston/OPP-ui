85b9e6e7224bfe70cd42088406a0ab9509043acc
Paul Caston <pcaston@arach.net.au>
2020-01-21 20:37:34 +0800

    import translations

--- src/resources/translations-metadata.ts
+++ src/resources/translations-metadata.ts
@@ -1,5 +1,6 @@
-import "../../build-translations/translationMetadata.js";
+import translationMetadata_  from "../../build-translations/translationMetadata.js";
 import { TranslationMetadata } from "../types.js";
+debugger;
 
 export const translationMetadata = (translationMetadata_ as any)
   .default as TranslationMetadata;
