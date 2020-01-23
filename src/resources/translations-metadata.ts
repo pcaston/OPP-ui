086bc02fdaf2edd616a930172ab415ed06f114f6
Paul Caston <paul.caston@iag.com.au>
2020-01-23 16:57:15 +0800

    added yaml-edit types

--- src/resources/translations-metadata.ts
+++ src/resources/translations-metadata.ts
@@ -1,6 +1,5 @@
 import translationMetadata_  from "../../build-translations/translationMetadata.js";
 import { TranslationMetadata } from "../types.js";
-debugger;
 
 export const translationMetadata = (translationMetadata_ as any)
   .default as TranslationMetadata;
